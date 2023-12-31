a75044e9b07fc8c91c60b3d7344b384d9b523e9c
Matt Crook <mattlcrook@icloud.com>
2024-01-01 10:39:41 +1300

    resolve breaking changes in package update

--- src/StandardNotesEditor.App/frontend/webpack.config.js
+++ src/StandardNotesEditor.App/frontend/webpack.config.js
@@ -1,29 +1,27 @@
-const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const {resolve} = require("path");
 
 module.exports = {
-    context: __dirname, entry: [path.resolve(__dirname, "src/index.js"), path.resolve(__dirname, "src/stylesheet.scss")], module: {
-        rules: [{
-            test: /\.(js|jsx)$/, exclude: /node_modules/, use: {
-                loader: "babel-loader"
+    context: __dirname,
+    entry: [resolve(__dirname, "src/index.js"), resolve(__dirname, "src/stylesheet.scss")],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            importLoaders: 1
+                        }
+                    },
+                    "postcss-loader",
+                    "sass-loader"
+                ]
             }
-        }, {
-            test: /\.s[ac]ss$/i, exclude: /node_modules/, use: [MiniCssExtractPlugin.loader, "css-loader", {
-                loader: "sass-loader"
-            },],
-        }]
-    }, output: {
-        path: path.resolve(__dirname, "../wwwroot/bundle"), filename: "vendor.min.js", library: "vendor"
-    }, plugins: [new MiniCssExtractPlugin({
-        filename: "styles.min.css"
-    }),], optimization: {
-        minimizer: [new OptimizeCssAssetsPlugin({
-            cssProcessorOptions: {
-                map: {
-                    inline: false, annotation: true,
-                },
-            },
-        }),],
+        ]
+    },
+    output: {
+        path: resolve(__dirname, "../wwwroot/bundle"), filename: "vendor.min.js", library: "vendor"
     }
 };
